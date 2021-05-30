606d5e5ea2e5b08ffd8357273bfb24d426b28284
alexpdev <alexpdev@protonmail.com>
2021-05-30 16:24:10 -0700

    updated licensing information: LGPL

--- minmax/MinMax.py
+++ minmax/MinMax.py
@@ -1,6 +1,24 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
+#############################################################################
+# MinMaxObj  extends builtin min and max functions.
+# Copyright (C) 2021 alexpdev
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+############################################################################
+
 """Objects and functions that provide a more robust way obtain min and max.
 
 Functions and Classes that help deal with minimum and maximum
---
+++ minmax/__init__.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#############################################################################
+# MinMaxObj  extends builtin min and max functions.
+# Copyright (C) 2021 alexpdev
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+############################################################################
+
+
+
+output = '''MinMaxObj  Copyright (C) 2021  alexpdev
+    This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type 'show c' for details.'''
+
+__version__ = "0.1.1"
+print("version:",__version__,"\n",output)
