de9025264606eb9679eb2c22ebecc8a7d1cba39b
Alex <alexpdev@protonmail.com>
2021-07-08 22:17:40 -0700

    addeed script temp

--- script.py
+++ script.py
@@ -1,45 +1,68 @@
-import sys
 import os
+import sys
 from ftplib import FTP, error_perm
+from time import time
 import dotenv
 
-home = "~"
-local_dir = os.path.join("/home/asp","HDisk","sync")
-remote_dir = "/storage/downloads/rtorrent"
-local_contents = os.listdir(local_dir)
+then = time()
+
+local = "/home/asp/HDisk/sync"
+remote = "/storage/downloads/rtorrent"
+
+host = os.environ.get("HOST")
+port = int(os.environ.get("PORT"))
+auth = os.environ.get("AUTH")
+user = "alexpdev"
+client = FTP()
+
+
+def syncremote(client,remote,local):
+    contents = os.listdir(local)
+    for path in client.nlst(remote):
+        name = os.path.split(path)[-1]
+        if name not in contents:
+            get_path(client,path,local)
+        else:
+            lpath = os.path.join(local,name)
+            if os.path.isdir(lpath):
+                find_incomplete(client,path,lpath)
+    return "Completed"
+
+def find_incomplete(client,remote,local):
+    lst = os.listdir(local)
+    for item in client.nlst(remote):
+        name = os.path.basename(item)
+        if name in lst:
+            path = os.path.join(local,name)
+            if os.path.isdir(path):
+                find_incomplete(client,item,path)
+        else:
+            get_path(client, item, local)
+
 
 
 def get_path(client,path,parent):
-    name = os.path.split(path[-1])
+    name = os.path.split(path)[-1]
     try:
         s = client.size(path)
         local = os.path.join(parent,name)
         with open(local,"wb") as fp:
             client.retrbinary(f'RETR {path}', fp.write)
     except error_perm:
+        client.dir(path)
         lst = client.nlst(path)
         child = os.path.join(parent,name)
         os.mkdir(child)
         for p in lst:
             get_path(client,p, child)
 
-    return
 
-host = os.environ.get("host")
-port = os.environ.get("port")
-passwd = os.environ.get("passwd")
-uname = os.environ.get("uname")
-
-
-client = FTP()
-client.login(host=host,port=port)
-client.login(user=uname,passwd=passwd)
-client.set_debuglevel(2)
-
-remote_contents = client.nlst(remote_dir)
-for path in remote_contents:
-    pathname = os.path.split[-1]
-    if pathname not in local_contents:
-        get_path(client,pathname,local_dir)
-
-print("complete")
+if __name__ == "__main__":
+    client.set_debuglevel(1)
+    client.connect(host=host,port=port)
+    client.login(user=user,passwd=auth)
+    client.pwd()
+    client.dir()
+    syncremote(client,remote,local)
+    now = time()
+    print("Time to complete ", now - then)
