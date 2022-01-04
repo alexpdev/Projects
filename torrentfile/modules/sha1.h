52d6e2bcedc20d6451dfa9c9fccffb1657894c0b
Alex <alexpdev@protonmail.com>
2022-01-03 16:31:57 -0800

    experimenting with hashing in c

---
+++ torrentfile/modules/sha1.h
@@ -0,0 +1,22 @@
+#ifndef SHA1_H
+#define SHA1_H
+#include "stdint.h"
+
+typedef struct
+{
+    uint32_t state[5];
+    uint32_t count[2];
+    unsigned char buffer[64];
+} SHA1_CTX;
+
+#define DIGEST_LENGTH 20
+
+void SHA1Init(SHA1_CTX * context);
+
+void SHA1Update(SHA1_CTX * context, const unsigned char *data, uint32_t len);
+
+void SHA1Final(unsigned char digest[DIGEST_LENGTH], SHA1_CTX * context);
+
+void SHA1(char *hash_out, const char *str, int len);
+
+#endif
