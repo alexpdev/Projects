1c8e0b64b21d0142f775332ae56a0066a972ef6b
Alex <alexpdev@protonmail.com>
2022-03-15 17:17:21 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bitprint",
+    version="0.1.0",
+    author="alexpdev",
+    author_email="alexpdev@protonmail.com",
+    packages=["bitprint"],
+    entry_points={"console-scripts":["bitprint:main"]},
+    description="convert integers to binary",
+)
