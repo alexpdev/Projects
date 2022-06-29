c7da7650e44d9c22e10f2cd8152c36983c417a6b
Alex <alexpdev@protonmail.com>
2022-06-29 00:14:11 -0700

    update

--- QFilter/window.py
+++ QFilter/window.py
@@ -1,19 +1,11 @@
 import sys
 import os
 import json
-
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
 
-
 DATADIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
-# URL
-# VIEWS
-# TITLE
-# COUNT
-# ADDED
-# RATING
 
 class TableWidget(QTableWidget):
 
@@ -21,23 +13,27 @@ class TableWidget(QTableWidget):
     itemReady = Signal([int, int, QTableWidgetItem])
     loadReady = Signal([list])
     rowReady = Signal([list])
+    resizeReady = Signal()
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setStyleSheet("font-size: 9pt;")
-        self.setColumnCount(6)
+        self.setColumnCount(5)
         self.setRowCount(0)
-        self.labels = ["TITLE", "VIEWS", "COUNT", "ADDED", "RATING", "URL"]
+        self.labels = ["TITLE", "VIEWS", "COUNT", "ADDED", "URL"]
         self.verticalHeader().setHidden(True)
         self.data = []
         self.setHorizontalHeaderLabels(self.labels)
         self.setHidden.connect(self.hide)
         self.itemReady.connect(self.addItem)
+        self.resizeReady.connect(self.resizeColumnsToContents)
 
     def setRow(self, items):
         index = self.rowCount()
         self.insertRow(index)
-        for i, item in enumerate(items):
+        for i, val in enumerate(items):
+            item = QTableWidgetItem(str(val), 0)
+            item.value = val
             self.setItem(index, i, item)
 
     def hide(self, row, truth):
@@ -46,7 +42,6 @@ class TableWidget(QTableWidget):
     def addItem(self, row, col, item):
         self.setItem(row, col, item)
 
-
     def lookup(self,text):
         for row in range(self.rowCount()):
             s = self.item(row,0).text()
@@ -54,7 +49,6 @@ class TableWidget(QTableWidget):
                 self.setRowHidden(row, True)
             else:
                 self.setRowHidden(row, False)
-
 
 class Worker(QObject):
 
@@ -77,7 +71,7 @@ class Worker(QObject):
             "month": 60*60*24*30,
             "year": 60*60*24*365
         }
-        self.labels = ["title", "views", "count", "added", "rating", "url"]
+        self.labels = ["title", "views", "count", "added","url"]
         self.ranges = {i:set() for i in self.labels if i not in ["title", "url"]}
 
     def run(self):
@@ -104,26 +98,63 @@ class Worker(QObject):
                             break
                     total = int(parts[0]) * val
                     v = total
-                if k == "rating":
-                    v = int(v[:-1])/100
                 if k in self.ranges:
                     self.ranges[k].add(v)
                 i = self.labels.index(k)
-                item = QTableWidgetItem(str(v), 0)
-                item.value = v
-                items[i] = item
+                items[i] = v
             self.rowReady.emit(items)
         for k,v in self.ranges.items():
             self.limitsChanged.emit(k,v)
-        self.table.setHorizontalHeaderLabels(self.table.labels)
+        self.table.resizeReady.emit()
         self.finished.emit()
 
-class Validator(QIntValidator):
+    def run2(self):
+        addedval = int(self.widget.addedbox.value())
+        text = list(self.widget.keywords.text())
+        print(text)
+        mapping = {
+            1: int(self.widget.viewbox.value()),
+            2: int(self.widget.countbox.value()),
+        }
+        for i in range(self.table.rowCount()):
+            hidden = False
+            for k,v in mapping.items():
+                if self.table.item(i, k).value < v:
+                    hidden = True
+                    self.table.setHidden.emit(i, True)
+                    break
+            if hidden: continue
+            if self.table.item(i, 3).value > addedval:
+                self.table.setHidden.emit(i,True)
+                continue
+            title = self.table.item(i,0).text().lower()
+            text = [i for i in text if i.isalpha()]
+            if len([i for i in text if i not in title]):
+                self.table.setHidden.emit(i, True)
+                continue
+            self.table.setHidden.emit(i, False)
+        self.finished.emit()
 
-    def fixup(self, text):
-        if not text or not text.isnumeric():
-            # self.parent().setText("0")
-            return "0"
+
+class LineFilter(QSpinBox):
+
+    validated = Signal()
+
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent=parent)
+        # self.setReadOnly(True)
+        # self.setButtonSymbols(self.PlusMinus)
+        self.setAccelerated(True)
+        self.setCorrectionMode(self.CorrectToNearestValue)
+        self.valueChanged.connect(self.validateInput)
+
+    def setRange(self, mn, mx):
+        self.setMinimum(mn)
+        self.setMaximum(mx)
+        self.setValue(mn)
+
+    def validateInput(self):
+        self.validated.emit()
 
 
 
@@ -139,25 +170,14 @@ class Window(QMainWindow):
         self.viewlabel = QLabel(parent=self)
         self.countlabel = QLabel(parent=self)
         self.addedlabel = QLabel(parent=self)
-        self.ratinglabel = QLabel(parent=self)
         self.keywordlabel = QLabel(parent=self)
         self.viewlabel.setText("Minimum Views")
         self.addedlabel.setText("Maximum Time")
         self.countlabel.setText("Minimum Count")
-        self.ratinglabel.setText("Minimum Rating")
         self.keywordlabel.setText("Keywords")
-        self.countbox = QLineEdit(parent=self)
-        self.countbox.setValidator(Validator())
-        self.countbox.setText("0")
-        self.addedbox = QLineEdit(parent=self)
-        self.addedbox.setValidator(Validator())
-        self.addedbox.setText("0")
-        self.viewbox = QLineEdit(parent=self)
-        self.viewbox.setValidator(Validator())
-        self.viewbox.setText("0")
-        self.ratingbox = QLineEdit(parent=self)
-        self.ratingbox.setValidator(Validator())
-        self.ratingbox.setText("0")
+        self.countbox = LineFilter(parent=self)
+        self.addedbox = LineFilter(parent=self)
+        self.viewbox = LineFilter(parent=self)
         self.keywords = QLineEdit(parent=self)
         self.titleslabel = QLabel(parent=self)
         self.table = TableWidget(parent=self)
@@ -169,8 +189,6 @@ class Window(QMainWindow):
         self.filtergrid.addWidget(self.viewbox, 0,3,1,1)
         self.filtergrid.addWidget(self.countlabel, 0,4,1,1)
         self.filtergrid.addWidget(self.countbox, 0,5,1,1)
-        self.filtergrid.addWidget(self.ratinglabel, 0,6,1,1)
-        self.filtergrid.addWidget(self.ratingbox, 0,7,1,1)
         self.filtergrid.addWidget(self.keywordlabel,1,0,1,1)
         self.filtergrid.addWidget(self.keywords, 1,1,-1,-1)
         self.layout.addLayout(self.filtergrid)
@@ -179,64 +197,53 @@ class Window(QMainWindow):
         self.layout.addWidget(self.button)
         self.setCentralWidget(self.central)
         self.button.clicked.connect(self.loadData)
-        self.viewbox.textChanged.connect(self.filter)
-        self.countbox.textChanged.connect(self.filter)
-        self.ratingbox.textChanged.connect(self.filter)
-        self.addedbox.textChanged.connect(self.filter)
+        self.viewbox.validated.connect(self.filter)
+        self.countbox.validated.connect(self.filter)
+        self.addedbox.validated.connect(self.filter)
         self.keywords.textChanged.connect(self.lookup)
-
+        self.threads = []
 
     def setLimits(self, label, seq):
         widgets = {"added": self.addedbox,
                    "views": self.viewbox,
                    "count": self.countbox,
-                   "rating": self.ratingbox}
+                   }
         box = widgets[label]
         mn,mx = min(seq), max(seq)
         if box == "rating":
             mn ,mx = mn*100, mx*100
-        box.validator().setBottom(mn)
-        box.validator().setTop(mx)
+        box.setRange(mn, mx)
 
     def filter(self):
-        addedval = int(self.addedbox.text())
-        kws = self.keywords.text()
-        mapping = {
-            1: int(self.viewbox.text()),
-            2: int(self.countbox.text()),
-            4: int(self.ratingbox.text())
-        }
-        for i in range(self.table.rowCount()):
-            hidden = False
-            for k,v in mapping.items():
-                if self.table.item(i, k).value < v:
-                    hidden = True
-                    self.table.setHidden.emit(i, True)
-                    break
-            if hidden: continue
-            if self.table.item(i, 3).value > addedval:
-                self.table.setHidden.emit(i,True)
-                continue
-            text = self.table.item(i,0).text().lower()
-            if len([j for j in kws.lower() if j not in text and j.isalpha()]):
-                self.table.setHidden.emit(i, True)
-            self.table.setHidden.emit(i, False)
+        worker = Worker(self, self.table)
+        thread = QThread()
+        self.threads.append((thread, worker))
+        worker.moveToThread(thread)
+        thread.started.connect(worker.run2)
+        worker.finished.connect(thread.quit)
+        thread.finished.connect(self.destroyThread)
+        thread.start()
+
+    def destroyThread(self):
+        for i, thread in enumerate(self.threads):
+            if thread[0].isFinished():
+                thread[0].deleteLater()
+                del self.threads[i]
 
     def loadData(self):
-        self.thread = QThread()
-        self.worker = Worker(self, self.table)
-        self.worker.moveToThread(self.thread)
-        self.worker.limitsChanged.connect(self.setLimits)
-        self.worker.rowReady.connect(self.table.setRow)
-        self.thread.started.connect(self.worker.run)
-        self.worker.finished.connect(self.thread.quit)
-        self.thread.finished.connect(self.thread.deleteLater)
-        self.thread.start()
+        thread = QThread()
+        worker = Worker(self, self.table)
+        self.threads.append((thread, worker))
+        worker.moveToThread(thread)
+        worker.limitsChanged.connect(self.setLimits)
+        worker.rowReady.connect(self.table.setRow)
+        thread.started.connect(worker.run)
+        worker.finished.connect(thread.quit)
+        thread.finished.connect(self.destroyThread)
+        thread.start()
 
     def lookup(self, text):
         self.table.lookup(text)
-
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
