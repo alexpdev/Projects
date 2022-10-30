f6555cb1b163487fa3cd5ec7a956d94cf5d0a9c8
Alex <alexpdev@protonmail.com>
2022-10-29 23:49:34 -0700

    Manager context

--- torrentmanager/manager.py
+++ torrentmanager/manager.py
@@ -1,5 +1,8 @@
 import pyben
 import os
+import json
+import atexit
+import asyncio
 from pathlib import Path
 from datetime import datetime
 from PySide6.QtWidgets import *
@@ -126,3 +129,128 @@ class Torrent:
 
     def __eq__(self, other):
         return other.name == self.name and other.path == self.path
+
+class Worker(QObject):
+
+    finished = Signal()
+    torrentCreated = Signal(Torrent)
+
+    def __init__(self, paths, manager, parent=None):
+        super().__init__(parent=parent)
+        self.paths = [Path(i) for i in paths]
+        self.manager = manager
+        self.threads = []
+        self.count = 0
+
+    def run(self):
+        for path in self.paths:
+            self.walk_path(path)
+        self.finished.emit()
+
+    def walk_path(self, root):
+        if root.is_file():
+            if root.suffix == ".torrent":
+                try:
+                    torrent = Torrent(root)
+                except PermissionError:
+                    print("Permission Error", root)
+                    return
+                self.torrentCreated.emit(torrent)
+                self.count += 1
+                if self.count and self.count % 50 == 0:
+                    print(self.objectName, " - Count: ", self.count)
+        elif root.is_dir():
+            try:
+                filelist = list(root.iterdir())
+            except PermissionError:
+                print("Permission Error", root)
+                return
+            for item in filelist:
+                self.walk_path(item)
+
+class TorrentManager(QObject):
+
+    torrentAdded = Signal(Torrent)
+
+    def __init__(self, backend, path=None):
+        super().__init__()
+        self.backend = backend(path=path)
+        self.torrents = asyncio.run(self.backend.get())
+        self.threads = []
+
+    def run_search(self, paths):
+        thread = QThread()
+        self.worker = Worker(paths, manager=self)
+        self.worker.moveToThread(thread)
+        self.worker.torrentCreated.connect(self.add_torrent)
+        self.worker.finished.connect(thread.quit)
+        thread.started.connect(self.worker.run)
+        thread.finished.connect(self.worker.deleteLater)
+        thread.finished.connect(thread.deleteLater)
+        thread.start()
+        self.threads.append(thread)
+
+    def add_torrent(self, torrent):
+        self.backend.store(torrent)
+        self.torrents.append(torrent)
+        self.torrentAdded.emit(len(self.torrents))
+
+class StorageBackend:
+
+    def __init__(self, *args, **kwargs):
+        Current.set(self)
+
+    def store(self, *items):
+        raise NotImplemented
+
+
+class JSONBackend(StorageBackend):
+
+    def __init__(self, path=None):
+        super().__init__(path=path)
+        self.path = path
+        if not self.path:
+            self.path = Path("./.torrentfilemanager/archive.json")
+            archive = self.path
+            create_path = []
+            while not os.path.exists(str(archive)):
+                create_path.append(archive.name)
+                archive = archive.parent
+            if len(create_path) > 1:
+                os.makedirs(os.path.join(archive, *create_path[:-1]))
+            json.dump([],open(self.path,'wt'))
+
+    def get(self):
+        data = json.load(open(self.path))
+        return data
+
+    def store(self, *items):
+        data = self.get()
+        item_list = store_items(items)
+        json.dump(data+item_list, open(self.path, 'wt', encoding='utf8'))
+
+
+def store_items(items):
+    items = []
+    for item in items:
+        item = item.to_dict()
+        items.append(item)
+    return items
+
+class Current:
+
+    _instance = None
+
+    @classmethod
+    def set(cls, value): cls._instance = value
+
+    @classmethod
+    def get(cls): return cls._instance
+
+def get_current_backend():
+    return Current.get()
+
+@atexit.register
+def save_archive():
+    backend = get_current_backend()
+    json.dump(backend.archive, open(backend.filepath, 'wt', encoding='utf8'))
